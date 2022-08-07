ca832284e4903c0d22f5cfe58b0a04a0c71524b8
thhan <thhan@cndfactory.com>
2022-08-07 19:09:55 +0900

    초기 세팅

---
+++ src/main/java/com/example/studymarketkurlyapi/StudyMarketKurlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.studymarketkurlyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMarketKurlyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyMarketKurlyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studymarketkurlyapi/StudyMarketKurlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studymarketkurlyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMarketKurlyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
